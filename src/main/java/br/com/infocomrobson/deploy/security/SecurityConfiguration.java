1ba389af6dc6423d6e18ca84ff4462ca89fb5622
Robson Farias <robsonfdfarias@gmail.com>
2022-08-21 23:17:44 -0300

    Security

---
+++ src/main/java/br/com/infocomrobson/deploy/security/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package br.com.infocomrobson.deploy.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    //configurar o login
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordENconder().encode("Manaus123!@#"))
+                .roles("USER")
+                .and()
+                .passwordEncoder(passwordENconder());
+    }
+
+    @Override
+    //configura a autorização
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/webjar/**").permitAll()
+                .antMatchers("/v2/api-docs/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/csrf").permitAll()
+                .antMatchers("/*.js").permitAll()
+                .antMatchers("/*.css").permitAll()
+                .antMatchers("/*.ico").permitAll()
+                .antMatchers("/*.png").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public PasswordEncoder passwordENconder() {
+        return new BCryptPasswordEncoder();
+    }
+}
